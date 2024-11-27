d8d0d6bbeed1336dc5b39c2e1f810c8e6a880893
ZQDesigned <2990918167@QQ.COM>
2024-11-27 16:38:16 +0800

    fix; clean cache from common module

--- services/common/bin/main/com/lnyynet/filecollect/common/exception/ApiException.kt
+++
@@ -1,6 +0,0 @@
-package com.lnyynet.filecollect.common.exception
-
-class ApiException(
-    val errorCode: Int,
-    override val message: String
-) : RuntimeException(message) 
--- services/common/bin/main/com/lnyynet/filecollect/common/util/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package com.lnyynet.filecollect.common.util
-
-import com.lnyynet.filecollect.common.model.Response
-import org.springframework.http.ResponseEntity
-
-fun <T> T.toResponse(): Response<T> = Response.success(this)
-
-fun <T> Response<T>.toResponseEntity(): ResponseEntity<Response<T>> = ResponseEntity.ok(this) 
